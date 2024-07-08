42d0613c0b2e39ef52f632a3cf11d1d71e27a999
Cameron Nazarko <cnazarko@Camerons-MacBook-Pro.local>
2024-07-08 02:49:31 -0400

    Initial commit

---
+++ contracts/Guestbook.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.24;
+
+contract Guestbook {
+    struct Entry {
+        string name;
+        string message;
+    }
+
+    Entry[] public entries;
+
+    function signGuestbook(string memory _name, string memory _message) public {
+        entries.push(Entry(_name, _message));
+    }
+
+    function getEntries() public view returns (Entry[] memory) {
+        return entries;
+    }
+}
